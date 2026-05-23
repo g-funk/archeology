3b01b6b37a7916c6a58bc8782711c98fe6689ea1
g-funk <toni.palm@gmail.com>
2026-05-23 15:54:32 +0300

    implement a custom TabBar

---
+++ src/Arkeology.Production.Client/scripts/ui/TabButton.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+
+public partial class TabButton : Button
+{
+	[Export] int TabIndex;
+	[Export] TabContainer TabContainer;
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		Pressed += () => TabContainer.CurrentTab = TabIndex;  
+	}
+}
